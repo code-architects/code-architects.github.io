c4abcab01d20e2225c444bc9c4d4d2150350be60
Code Architects <68094713+code-architects@users.noreply.github.com>
2020-07-16 23:20:31 +0530

    Add files via upload

---
+++ scripts/mousecursor.js
@@ -0,0 +1,24 @@
+const button = document.querySelector(".scrolltop");
+window.onscroll = () => {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.opacity = "1";
+        button.style.pointerEvents = "all";
+
+    } else {
+        button.style.opacity = "0";
+        button.style.pointerEvents = "none";
+    }
+}
+function scrolltotop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
+let mouseCursor = document.querySelector(".cursor");
+
+window.addEventListener('mousemove', (e) => {
+    mouseCursor.style.top = e.pageY + 'px';
+    mouseCursor.style.left = e.pageX + 'px';
+})
---
+++ scripts/postlist.js
@@ -0,0 +1,126 @@
+let myapp = angular.module('myapp', []);
+
+myapp.controller('myappController', ['$scope', function ($scope) {
+    $scope.previewlists = [
+        {
+            name: "Button effect 3",
+            src: "https://code-architects.github.io/button-effect-3/",
+            lang: "CSS"
+        },
+        {
+            name: "Sibling Fade",
+            src: "https://code-architects.github.io/sibling-fade/",
+            lang: "CSS"
+        },
+        {
+            name: "Typewriter Effect JS",
+            src: "https://code-architects.github.io/typewriter-effect-js/",
+            lang: "CSS JS"
+        },
+        {
+            name: "Typewriter Effect CSS",
+            src: "https://code-architects.github.io/typewriter-effect-css/",
+            lang: "CSS"
+        },
+        {
+            name: "Gradient Border",
+            src: "https://code-architects.github.io/gradient-border/",
+            lang: "CSS"
+        },
+        {
+            name: "Button effect 1",
+            src: "https://code-architects.github.io/button-effect-1/",
+            lang: "CSS"
+        },
+        {
+            name: "Gradient Background Animation",
+            src: "https://code-architects.github.io/gradient-background-animation/",
+            lang: "CSS"
+        },
+        {
+            name: "3D Button",
+            src: "https://code-architects.github.io/3d-button/",
+            lang: "CSS"
+        },
+        {
+            name: "Create Multiple Borders",
+            src: "https://code-architects.github.io/create-multiple-borders/",
+            lang: "CSS"
+        },
+        {
+            name: "Generate Strong Password",
+            src: "https://code-architects.github.io/generate-strong-password/",
+            lang: "JS"
+        },
+        {
+            name: "Text Selection",
+            src: "https://code-architects.github.io/text-selection/",
+            lang: "CSS"
+        },
+        {
+            name: "Loading Animation",
+            src: "https://code-architects.github.io/loading-animation/",
+            lang: "CSS"
+        },
+        {
+            name: "Countdown Timer",
+            src: "https://code-architects.github.io/countdown-timer/",
+            lang: "CSS JS"
+        },
+        {
+            name: "Create Ribbon",
+            src: "https://code-architects.github.io/create-ribbon/",
+            lang: "CSS"
+        },
+        {
+            name: "Scroll to Top",
+            src: "https://code-architects.github.io/scroll-to-top/",
+            lang: "CSS JS"
+        },
+        {
+            name: "Parallax Scrolling",
+            src: "https://code-architects.github.io/parallax-scrolling/",
+            lang: "CSS"
+        },
+        {
+            name: "Text to Speech",
+            src: "https://code-architects.github.io/text-to-speech/",
+            lang: "JS"
+        },
+        {
+            name: "Baffle Text",
+            src: "https://code-architects.github.io/baffle-text/",
+            lang: "JS"
+        },
+        {
+            name: "Card Hover effect",
+            src: "https://code-architects.github.io/card-hover-effect/",
+            lang: "CSS"
+        },
+        {
+            name: "Creative skew border",
+            src: "https://code-architects.github.io/creative-skew-border/",
+            lang: "CSS"
+        },
+        {
+            name: "Pure CSS text Animation",
+            src: "https://code-architects.github.io/pure-css-text-animation/",
+            lang: "CSS"
+        },
+        {
+            name: "Stop form Refreshing page on Submit",
+            src: "https://code-architects.github.io/stop-form-refreshing-page-on-submit/",
+            lang: "CSS JS"
+        },
+        {
+            name: "Show/Hide Password",
+            src: "https://code-architects.github.io/show-hide-password/",
+            lang: "CSS JS"
+        },
+        {
+            name: "3D Cube",
+            src: "https://code-architects.github.io/3d-cube/",
+            lang: "CSS"
+        }
+    ];
+}]);
